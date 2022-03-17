9af28976caad8eeb547b91a70461456ddc3fbe60
vkingmaker <vkingmaker1@gmail.com>
2022-03-17 02:47:06 +0100

    ft: calculateS corresponding fee
    This PR calculate the fee applicable to a transaction based on specific fee configuration
    
    Delivers #feeCalculator

--- server/services/fcs/index.js
+++ server/services/fcs/index.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const Response = require("../../../utils/constants/Response"),
-			RedisClient = require("../../../lib/redis");
+			RedisClient = require("../../../lib/redis"),
+			ComputeService = require("../ComputeService");
 
 /**
  * Handles every business logic that concerns Fee configuration spec
  */
 module.exports = class FcsService {
   /**
-   * Creats the parsed FeeConfigurationSpec
+   * Creates the parsed FeeConfigurationSpec
    * @property {Function} create  parse and save FeeConfigurationSpec in an in-memory Database
    * @returns {Object} returns the parsed FeeConfigurationSpec object
    */
@@ -17,7 +19,7 @@ module.exports = class FcsService {
           availableTypesOfPayment = {};
 
 			for(const data of fcsArr) {
-				const [, currency,, paymentType] = data.split(':')[0].split(" ");
+				const [, currency,, paymentType] = data.trim().split(':')[0].split(" ");
 				if(!allowedCurrencies.includes(currency)) allowedCurrencies.push(currency);
 				if(!availableTypesOfPayment[paymentType.substr(0, paymentType.indexOf("("))]) {
 					availableTypesOfPayment[paymentType.substr(0, paymentType.indexOf("("))] = [data];
@@ -27,9 +29,77 @@ module.exports = class FcsService {
 			}
 
       await RedisClient.set('currency', JSON.stringify(allowedCurrencies));
-      await RedisClient.set('paymentType', JSON.stringify(availableTypesOfPayment));
+      await RedisClient.set('paymentTypes', JSON.stringify(availableTypesOfPayment));
 
       return { allowedCurrencies, availableTypesOfPayment };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+	/**
+   * Compute transaction fee
+   * @property {Function} compute calculates the transaction fee based on the parsed FeeConfigurationSpec in the Database
+   * @returns {Object} returns the result from the compute transaction fee
+   */
+  static async compute(payload) {
+    try {
+			const { 
+				PaymentEntity: {
+					ID,
+					Issuer,
+					Brand,
+					Number,
+					SixID,
+					Type: UsersPayment,
+					Country
+				}, 
+				BearsFee,
+				Currency,
+				Amount
+			} = payload;
+			
+			const LOCAL_COUNTRY = process.env.LOCAL_COUNTRY ? process.env.LOCAL_COUNTRY : 'NG';
+			const allowedCurrency = JSON.parse(await RedisClient.get('currency'));
+
+			if(!allowedCurrency.includes(`${Currency}`)) throw { 
+				message: `No fee configuration for ${Currency} transactions`,
+				statusCode: Response.HTTP_UNPROCESSABLE_ENTITY 
+			};
+
+			const paymentTypes =  JSON.parse(await RedisClient.get('paymentTypes'));
+			let mostSpecificSpec;
+
+			const applicableSpecs = paymentTypes[`${UsersPayment}`].filter(payment => {
+				const [,,, feeEntity] = payment.trim().split(":")[0].split(" ");
+				const entityProperty = feeEntity
+																.substr(feeEntity.indexOf("("))
+																.trim()
+																.split("").slice(1, -1)
+																.join("");
+
+				return [ID, Issuer, Brand, Number, SixID].includes(entityProperty) || entityProperty === "*";
+				
+			}).filter(spec => {
+				const [,, feeLocale] = spec.trim().split(":")[0].split(" ");
+				const locale = Country === LOCAL_COUNTRY ? "LOCL":"INTL"
+				return feeLocale === locale;
+			});
+
+			
+			if(!applicableSpecs.length) mostSpecificSpec = paymentTypes["*"][0]
+			else {
+				for(const spec of applicableSpecs) {
+					let numberOfAskteris = -1;
+					if((spec.split("*").length - 1) > numberOfAskteris) {
+						numberOfAskteris = spec.trim().split("*").length - 1;
+						mostSpecificSpec = spec;
+					}
+				}
+			}
+			
+			return await ComputeService.calculate(mostSpecificSpec, { Amount, BearsFee });
+
     } catch (error) {
       throw error;
     }
